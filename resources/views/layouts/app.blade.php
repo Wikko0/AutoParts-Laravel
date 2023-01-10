e981162bed06f6df9abdf8d73f497eafba4bdb84
Victor Minchev <victor.minchew@gmail.com>
2023-01-10 23:58:58 +0200

    JS assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -854,10 +854,10 @@
 
 <!-- ******** JS Files ******** -->
 <!-- Plugin JS -->
-<script src="assets/js/plugins.js"></script>
+<script src="{{asset('assets/js/plugins.js')}}"></script>
 
 <!-- Main JS -->
-<script src="assets/js/main.js"></script>
+<script src="{{asset('assets/js/main.js')}}"></script>
 
 </body>
 </html>
