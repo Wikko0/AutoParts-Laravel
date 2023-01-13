14c1337c7e57f39249d358c3bba0ae4e3b2da8cc
Victor Minchev <victor.minchew@gmail.com>
2023-01-13 19:29:13 +0200

    Models & Migrations

---
+++ database/migrations/2023_01_13_171157_create_order_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('adress1', 255);
+            $table->string('adress2', 255);
+            $table->string('city', 255);
+            $table->string('zipcode', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
